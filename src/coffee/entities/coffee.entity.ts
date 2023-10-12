76e9bc6a70216809e1d9f4679b8c40d2b1037e7e
developer <ch9716@naver.com>
2023-10-12 18:09:16 +0900

    entity 컬럼 정의

---
+++ src/coffee/entities/coffee.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'coffees' })
+export class Coffee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  type: string[];
+
+  @Column()
+  image: string;
+
+  @Column()
+  sizes: string[];
+
+  @Column()
+  price: number[];
+
+  @Column()
+  stars: number[];
+
+  @Column()
+  likes: boolean;
+
+  @Column()
+  product: { id: number; size: string; price: number; active: boolean }[];
+}
