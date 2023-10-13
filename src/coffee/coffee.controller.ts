c69898b852fc1c0b77f2e0927f920f25ede5d096
developer <ch9716@naver.com>
2023-10-13 10:28:50 +0900

    coffee 컨트롤러 구현

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,1 +1,18 @@
-export {};
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CoffeeService } from './coffee.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+
+@Controller('coffee')
+export class CoffeeController {
+  constructor(private coffeeService: CoffeeService) {}
+
+  @Post()
+  create(@Body() dto: CreateCoffeeDto) {
+    return this.coffeeService.create(dto);
+  }
+
+  @Get()
+  findAll() {
+    return this.coffeeService.findAll();
+  }
+}
